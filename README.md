b9d1c13bf900790d05e61fffd9fce66784b82130
speeedoops <speedoops@live.com>
2021-09-01 17:43:23 +0800

    refactor: remove code and message from GraphQL response

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # go-gqlrest
 Schema first GraphQL and REST API Server, based on 99designs/gqlgen.
 
-[demo](https://github.com/speedoops/go-gqlrest-example)
+[example project](https://github.com/speedoops/go-gqlrest-demo)
+
--- handlerx/utils.go
+++ handlerx/utils.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/errcode"
@@ -27,36 +28,51 @@ func statusFor(errs gqlerror.List) int {
 	}
 }
 
-type jsonResponse struct {
+// RESTResponse is response struct for RESTful API call
+// @see graphql.Response
+type RESTResponse struct {
 	Code    int             `json:"code"`
 	Data    json.RawMessage `json:"data"`
-	Errors  gqlerror.List   `json:"errors,omitempty"`
 	Message string          `json:"message,omitempty"`
 }
 
 func writeJSON(w io.Writer, r *graphql.Response, isRESTful bool) {
-	response := &jsonResponse{
-		Code:   200,
-		Errors: r.Errors,
-		Data:   r.Data,
+	// 1. For GraphQL API
+	if !isRESTful {
+		b, err := json.Marshal(r)
+		if err != nil {
+			panic(err)
+		}
+		_, err = w.Write(b)
+		if err != nil {
+			//logx.Errorf("an io write error occurred: %v", err)
+			panic(err)
+		}
+		return
 	}
 
-	if isRESTful && len(r.Data) > 0 {
-		// 1. unmarshal respose data to map
+	// 2. For RESTful API
+	response := &RESTResponse{
+		Code: 200,
+		Data: r.Data,
+	}
+
+	if len(r.Data) > 0 {
+		// a. unmarshal respose data to map
 		var m map[string]json.RawMessage
 		err := json.Unmarshal(r.Data, &m)
 		if err != nil {
 			panic(err)
 		}
 
-		// 2. get first key-value pair of the map
+		// b. get first key-value pair of the map
 		var k string
 		var v json.RawMessage
 		for k, v = range m {
 			break // it's ok to break here, because graphql response data will have only one top struct member
 		}
 
-		// 3. mapping or squashing
+		// c. mapping or squashing
 		if len(v) > 0 && v[0] == '[' {
 			// if it is a slice, change member name to 'list'
 			delete(m, k)
@@ -69,24 +85,16 @@ func writeJSON(w io.Writer, r *graphql.Response, isRESTful bool) {
 	}
 
 	if len(r.Errors) > 0 {
-		var code errcode.ErrorKind = errcode.KindUser
+		code, msgs := "500", []string{}
 		for _, e := range r.Errors {
-			if e.Extensions != nil {
-				if n, ok := e.Extensions["code"]; ok {
-					data, ok := n.(string)
-					if ok {
-						codeNum, _ := strconv.Atoi(data)
-						if codeNum > 0 {
-							code = errcode.ErrorKind(codeNum)
-							break
-						}
-					}
-				}
+			if n, ok := e.Extensions["code"]; ok {
+				code, _ = n.(string)
 			}
+			msgs = append(msgs, e.Message)
 		}
 
-		response.Code = int(code)
-		response.Message = r.Errors.Error()
+		response.Code, _ = strconv.Atoi(code)
+		response.Message = strings.Join(msgs, "; ")
 	}
 
 	b, err := json.Marshal(response)
