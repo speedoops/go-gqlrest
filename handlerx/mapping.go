9d1200e2ddeaa5eb5d85463f459acb8535c78824
speeedoops <speedoops@live.com>
2022-03-28 15:11:43 +0800

    chore: mark todo items, should be refactored

--- handlerx/mapping.go
+++ handlerx/mapping.go
@@ -210,7 +210,7 @@ func formatArgValueToGraphQL(underlayingType string, k string, v interface{}) (s
 	switch underlayingType {
 	case "Boolean", "Int", "Float":
 		return fmt.Sprintf(`%v`, v), nil
-	case "ID", "String", "Time", "IP", "IPRange", "MAC":
+	case "ID", "String", "Time", "IP", "IPRange", "MAC": // TODO: 新增基于string的scalar类型时，这里必须同步添加
 		return fmt.Sprintf("%q", v), nil
 	default:
 		if typeKind, ok := typeName2TypeKinds[underlayingType]; ok {
